0d79c4d6a73c96f4199c991a56f875d766f3e984
Zakirkhan95 <zk065748@gmail.com>
2024-12-17 16:35:49 +0530

    day6

---
+++ day6/list.py
@@ -0,0 +1,8 @@
+#python program to convert into a list
+
+thistuple=("apple","banana","cherry")
+y=list(thistuple)
+y[1]="kiw"
+x=tuple(y)
+print(x)
+
---
+++ day6/max&min.py
@@ -0,0 +1,8 @@
+#python program to find the Max and Min in the Tuple
+numbers = (7, 4, 20, 3, 8)
+
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
